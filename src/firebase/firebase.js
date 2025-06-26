f90d4622661e1b55c708c823e540d61ebb7a9e82
xganeryex <mnhngt126735@gmail.com>
2025-06-26 21:32:10 +0700

    feat: setup Firebase Authentication (email/password)

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 // src/firebase/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; //
 
 const firebaseConfig = {
   apiKey: "AIzaSyAtsgyCbjESUzTlRY-o7ywVMEpC5bwafg",
@@ -13,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 export { db };
